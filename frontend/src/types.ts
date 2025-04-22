6c2d2f0319a246183f530f4b2c1805141c8b6f41
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-22 17:04:29 +0300

    Update types.ts

---
+++ frontend/src/types.ts
@@ -0,0 +1,11 @@
+export interface Freelancer {
+  id: string;
+  name: string;
+  blurb: string;
+  bestThings: string[];
+  location: string;
+  contact: string;
+  latitude: number;
+  longitude: number;
+  createdAt: string;
+}

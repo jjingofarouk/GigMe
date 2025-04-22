e0c0629822519cdb821724491a938de98109af7d
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-22 21:58:58 +0000

    initiaitve

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://obscure-space-capybara-5g4jpq56rqww3vw77-3001.app.github.dev/';
 
 export const getFreelancers = () =>
   fetch(`${BASE_URL}/freelancers`).then((res) => res.json());

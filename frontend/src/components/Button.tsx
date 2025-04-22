8454d394a12dc7f38c5cb2f95e4454d0246718e8
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-22 16:47:00 +0300

    Update Button.tsx

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,16 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
+  return (
+    <button
+      className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;

1da3cc95d9addd14e38049e6b410b0482b5b67ed
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-28 01:37:28 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+// src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CreatePin from './pages/CreatePin';
 import FreelancerProfile from './pages/FreelancerProfile';
+import Browse from './pages/Browse';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -10,23 +14,18 @@ const App: React.FC = () => {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
-        {/* Header with navigation */}
         <Header />
-        {/* Main content area */}
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/create" element={<CreatePin />} />
             <Route path="/freelancer/:id" element={<FreelancerProfile />} />
-            {/* Placeholder routes for Header navigation links */}
-            <Route path="/freelancers" element={<NotFound />} />
-            <Route path="/about" element={<NotFound />} />
-            <Route path="/contact" element={<NotFound />} />
-            {/* Catch-all route for undefined paths */}
+            <Route path="/freelancers" element={<Browse />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
-        {/* Footer with links and contact info */}
         <Footer />
       </div>
     </Router>
